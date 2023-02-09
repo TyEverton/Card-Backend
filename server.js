0f548b2247618aca20a701ab9a1c7f1cb4b367a5
Ty <everton_915@yahoo.com>
2023-02-08 18:14:37 -0700

    added axios.get and app.get to send to frontend

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3015
 const router = express.Router()
+const admin = require('firebase-admin')
+const serviceAccount = require('./serviceAccountKey.json')
+const axios = require('axios')
 
-app.get('/usersBackEnd', (req, res) => {
-  // res.send('Hello, World!')
-  res.send([
-    {
-      id: 1,
-      username: 'River',
-    },
-    {
-      id: 2,
-      username: 'Mabel',
-    },
-  ])
+axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+  let userData = response.data
+
+  app.get('/usersBackEnd', (req, res) => {
+    res.send(userData)
+  })
+})
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'firebase-adminsdk-jbeuy@card-app-a32d0.iam.gserviceaccount.com',
 })
 
 app.listen(port, () => {
